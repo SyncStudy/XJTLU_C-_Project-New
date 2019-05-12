4c3e3bef7a3a56fb81c1c9d9c120bec71a3e52e6
YiboShan <50333757+YiboShan@users.noreply.github.com>
2019-05-12 21:26:13 +0800

    Add files via upload

--- 主程序.cpp
+++ 主程序.cpp
@@ -6,14 +6,46 @@
 #include<Windows.h>
 #include<fstream>
 #include"customer.h"
+#include"moreFunctionAboutCustomer.cpp"
+#include"Admin.h"
 using namespace std;
+
+int adminLogOn()
+{
+	string adminUsername, adminPassword;
+	int adminLogOnFlag = 0;
+	system("cls");
+	cout << "Administrator:" << endl;
+	cout << "Please enter your username and password to log on." << endl;
+	cout << "Username:";
+	getline(cin, adminUsername);
+	cin.sync();
+	cout << endl;
+	cout << "Password:";
+	getline(cin, adminPassword);
+	cin.sync();
+	if (admin.returnUsername() == adminUsername)
+	{
+		if (admin.returnPassword() == adminPassword)
+		{
+			cout << "You have logged on successfully!" << endl;
+			adminLogOnFlag = 1;
+		}
+	}
+	if (adminLogOnFlag == 0)
+	{
+		cout << "Your username or password is not correct." << endl;
+	}
+	return adminLogOnFlag;
+}
 
 int main()
 {
+	int i = 0;
 	int identificationSelection, customerSelection;
-	int telephoneNumber;
-	string username, password;
-	int identificationFlag = 0;
+	int telephoneNumber, customerID;
+	string adminUsername, adminPassword;
+	int identificationFlag = 0, adminLogOnFlag=0, customerLogOnFlag=0;
 	cout << "Welcome to the movie ticket booking system!" << endl;
 	cout << endl;
 	while (identificationFlag == 0)
@@ -28,16 +60,7 @@ int main()
 		switch (identificationSelection)
 		{
 		case 1:
-			system("cls");
-			cout << "Administrator:" << endl;
-			cout << "Please enter your username and password to log on." << endl;
-			cout << "Username:";
-			getline(cin, username);
-			cin.sync();
-			cout << endl;
-			cout << "Password:";
-			getline(cin, password);
-			cin.sync();
+			adminLogOnFlag = adminLogOn();
 			break;
 			/*Not finish*/
 
@@ -56,7 +79,24 @@ int main()
 			}
 			else
 			{
-				useCustomerAccountLog();
+				cout << "Please enter your telephone number:" << endl;
+				cin >> telephoneNumber;
+				for (i = 0; i < 50; i++)
+				{
+					if (customer[i].returnTel() == telephoneNumber)
+					{
+						cout << "You have logged on successfully!" << endl;
+						customerID = i + 1;
+						cout << "Your ID number is " << customerID << "." << endl;
+						customerLogOnFlag = 1;
+						break;
+					}
+				}
+				if (customerLogOnFlag == 0)
+				{
+					cout << "Your telephone number is not correct." << endl;
+					Sleep(1000);
+				}
 			}
 			break;
 			/*Not finish*/
@@ -69,6 +109,16 @@ int main()
 			cout << "Goodbye!" << endl;
 			return 0;
 			break;
+		}
+
+		if (adminLogOnFlag == 1)
+		{
+			cout << "Not finish";
+		}
+
+		if (customerLogOnFlag == 1)
+		{
+			cout << "Not finish";
 		}
 	}
 	return 0;
